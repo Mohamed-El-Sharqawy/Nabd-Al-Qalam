d228fef65b7fabf59de0c67c92cda0462d7f0aca
omar24_debug <oi204158@gmail.com>
2023-09-15 01:24:46 +0300

    ok

--- src/Components/Header/MiddNav/MiddNav.jsx
+++ src/Components/Header/MiddNav/MiddNav.jsx
@@ -1,6 +1,7 @@
 import './middnav.css'
 import { HiOutlineShoppingBag } from 'react-icons/Hi'
 import { BsSearch } from 'react-icons/Bs'
+import {BsFillPersonCheckFill} from 'react-icons/Bs'
 const MiddNav = () => {
 return (
     <>
@@ -9,6 +10,13 @@ return (
                 <div className="logo">
                     <a href="#"><img src="/src/assets/Logo_1.jpg" alt="" /></a>
                 </div>
+                <div className="leftlinks-search">
+                    <ul className='links-search'>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
                 <div className="search">
                     <input type="text" placeholder='Search Here..' />
                     <span><BsSearch /></span>
@@ -16,6 +24,8 @@ return (
                 <div className="cart">
                     <span><HiOutlineShoppingBag /></span>
                     <span className='count'>0</span>
+                    <BsFillPersonCheckFill />
+                    <a href="#"><img src="/src/assets/uae.jpg" alt="" /></a>
                 </div>
             </div>
         </div>
