2c5463064b3e139046795753f48466038f67d543
elsharqawi <dev.elbehery@gmail.com>
2023-09-15 15:41:56 +0300

    add books page responsive styles

--- src/pages/AddBooks/AddBooks.jsx
+++ src/pages/AddBooks/AddBooks.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
+import "./addBooks.css";
 
 const AddBooks = () => {
   const [book, setBook] = useState({
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home";
 import Contact from "../pages/Contact";
-import AddBooks from "../pages/AddBooks";
+import AddBooks from "../pages/AddBooks/AddBooks";
 
 export const router = createBrowserRouter([
   {
