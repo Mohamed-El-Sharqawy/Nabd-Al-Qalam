a9238bb1ffe87e7fd8ffecc3718be0f3b0706042
elsharqawi <dev.elbehery@gmail.com>
2023-10-16 15:00:02 +0300

    payment methods logo

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -35,6 +35,34 @@ const Footer = () => {
           <MailIcon /> N.alqalam.p.d@gmail.com
         </li>
       </ul>
+      <ul className="payment-methods">
+        <li>
+          <img
+            width={75}
+            height={75}
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/MasterCard_Logo.svg/2560px-MasterCard_Logo.svg.png"
+            alt="mastercard logo"
+          />
+          <img
+            width={75}
+            height={75}
+            src="https://upload.wikimedia.org/wikipedia/commons/d/d6/Visa_2021.svg"
+            alt="visa logo"
+          />
+          <img
+            width={75}
+            height={75}
+            src="https://cdn-icons-png.flaticon.com/512/196/196566.png"
+            alt="paypal logo"
+          />
+          <img
+            width={75}
+            height={75}
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Stripe_Logo%2C_revised_2016.svg/1280px-Stripe_Logo%2C_revised_2016.svg.png"
+            alt="stripe logo"
+          />
+        </li>
+      </ul>
     </footer>
   );
 };
