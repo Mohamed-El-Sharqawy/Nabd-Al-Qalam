3e85e615676ab244bf10322a43e3cb9e17f7f3ce
elsharqawi <dev.elbehery@gmail.com>
2023-10-24 13:16:13 +0300

    popup overlay added

--- src/Components/Popup/Popup.jsx
+++ src/Components/Popup/Popup.jsx
@@ -17,71 +17,77 @@ const Popup = ({ setPopup, popup, chosenBook }) => {
   };
 
   return (
-    <div
-      style={
-        lang === "ar"
-          ? { flexDirection: "row-reverse" }
-          : { flexDirection: "row" }
-      }
-      className={`popup ${popup ? "show" : "hidden"}`}
-    >
+    <>
       <div
-        className="left-popup"
-        style={lang === "ar" ? { textAlign: "right" } : { textAlign: "left" }}
+        style={
+          lang === "ar"
+            ? { flexDirection: "row-reverse" }
+            : { flexDirection: "row" }
+        }
+        className={`popup ${popup ? "show" : "hidden"}`}
       >
-        <p className="title">
-          {lang == "en" ? chosenBook.enTitle : chosenBook.arTitle}
-        </p>
-        <h4 className="price" dir={lang === "en" ? "ltr" : "rtl"}>
-          {chosenBook.price} {lang == "en" ? "AED" : "درهم"}
-        </h4>
-        <p className="overview">
-          {lang == "en" ? chosenBook.enDescription : chosenBook.arDescription}
-        </p>
-        <div className="btnpopup">
-          <button className="btnclick" onClick={() => decrease()}>
-            -
-          </button>
-          <span className="counter-product">{value}</span>
-          <button className="btnclick" onClick={() => increase()}>
-            +
-          </button>
-        </div>
-        <div className="addcart">
-          <button
-            onClick={() => {
-              setPopup(false);
-              for (let i = 0; i < value; i++) {
-                dispatch(addToCart(chosenBook));
-                dispatch(getTotal());
-              }
+        <div
+          className="left-popup"
+          style={lang === "ar" ? { textAlign: "right" } : { textAlign: "left" }}
+        >
+          <p className="title">
+            {lang == "en" ? chosenBook.enTitle : chosenBook.arTitle}
+          </p>
+          <h4 className="price" dir={lang === "en" ? "ltr" : "rtl"}>
+            {chosenBook.price} {lang == "en" ? "AED" : "درهم"}
+          </h4>
+          <p className="overview">
+            {lang == "en" ? chosenBook.enDescription : chosenBook.arDescription}
+          </p>
+          <div className="btnpopup">
+            <button className="btnclick" onClick={() => decrease()}>
+              -
+            </button>
+            <span className="counter-product">{value}</span>
+            <button className="btnclick" onClick={() => increase()}>
+              +
+            </button>
+          </div>
+          <div className="addcart">
+            <button
+              onClick={() => {
+                setPopup(false);
+                for (let i = 0; i < value; i++) {
+                  dispatch(addToCart(chosenBook));
+                  dispatch(getTotal());
+                }
 
-              toast.success("Added to The Cart", {
-                position: "top-right",
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "light",
-              });
-            }}
-          >
-            {lang == "en" ? "Add To Cart" : "اضف الي العربة"}
-          </button>
+                toast.success("Added to The Cart", {
+                  position: "top-right",
+                  autoClose: 5000,
+                  hideProgressBar: false,
+                  closeOnClick: true,
+                  pauseOnHover: true,
+                  draggable: true,
+                  progress: undefined,
+                  theme: "light",
+                });
+              }}
+            >
+              {lang == "en" ? "Add To Cart" : "اضف الي العربة"}
+            </button>
+          </div>
+        </div>
+        <div className="right-popup">
+          <img
+            src={chosenBook.img}
+            alt={lang == "en" ? chosenBook.enTitle : chosenBook.arTitle}
+          />
+        </div>
+        <div className="close">
+          <CancelIcon className="close" onClick={() => setPopup(false)} />
         </div>
       </div>
-      <div className="right-popup">
-        <img
-          src={chosenBook.img}
-          alt={lang == "en" ? chosenBook.enTitle : chosenBook.arTitle}
-        />
-      </div>
-      <div className="close">
-        <CancelIcon className="close" onClick={() => setPopup(false)} />
-      </div>
-    </div>
+      <div
+        onClick={() => setPopup(false)}
+        className={`popup-overlay-hidden ${popup && "popup-overlay-show"}`}
+      ></div>
+    </>
   );
 };
 
