640ee36f917c1c575ece964d320c600560901793
elsharqawi <dev.elbehery@gmail.com>
2023-10-24 09:49:43 +0300

    done

--- src/pages/AddBooks/AddBooks.jsx
+++ src/pages/AddBooks/AddBooks.jsx
@@ -29,7 +29,6 @@ const AddBooks = () => {
       headers: {
         "Access-Control-Allow-Headers":
           "access-control-allow-headers,access-control-allow-methods,access-control-allow-origin,content-type",
-        "Access-Control-Allow-Origin": "https://nabd-al-qalam.vercel.app",
         "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, DELETE",
         "Access-Control-Allow-Origin": "*",
       },
