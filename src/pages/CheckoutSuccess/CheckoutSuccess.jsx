22231b6b2f9b4d0d29e943b3cb03d5622f1f5728
elsharqawi <dev.elbehery@gmail.com>
2024-01-19 23:40:54 +0200

    add success page

--- src/pages/CheckoutSuccess/CheckoutSuccess.jsx
+++ src/pages/CheckoutSuccess/CheckoutSuccess.jsx
@@ -1,3 +1,37 @@
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import "./checkoutSuccess.css";
+
 export default function CheckoutSuccess() {
-  return <h2>Checkout Success</h2>;
+  const { lang } = useSelector((state) => state.lang);
+
+  return (
+    <section
+      style={
+        lang === "en"
+          ? { flexDirection: "row" }
+          : { flexDirection: "row-reverse" }
+      }
+      className="checkout-success"
+    >
+      <div>
+        <h1>
+          {lang === "en" ? "Thank Your for Ordering" : "شكرا لطلبك من متجرنا"}
+        </h1>
+        <Link className="main-btn" to="/books">
+          {lang === "en" ? "Explore More" : "اكتشف المزيد"}
+        </Link>
+      </div>
+      <div>
+        <img
+          src={
+            "https://res.cloudinary.com/dxfphp6to/image/upload/v1705699287/nabdu_al_qalam/checkout-success_uc678b.svg"
+          }
+          width={400}
+          height={400}
+          alt="Checkout Success Image"
+        />
+      </div>
+    </section>
+  );
 }
