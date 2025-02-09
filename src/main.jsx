1bd4e10953ebb270fc992ecc38f51085e54887b6
Omar Ismail <oi204158@gmail.com>
2025-02-09 23:55:27 +0000

    Add vercel analytics and speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,6 @@ import { store } from "./features/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <Provider store={store}>
-      <RouterProvider router={router}></RouterProvider>
+      <RouterProvider router={router} />
     </Provider>
 );
